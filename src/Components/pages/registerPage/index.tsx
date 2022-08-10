cb8364b4891ac3e21e77988801c83f07891a32ee
Anjani3 <akmitthv3@gmail.com>
2022-08-11 00:39:40 +0530

    Add changes in register component

--- src/Components/pages/registerPage/index.tsx
+++ src/Components/pages/registerPage/index.tsx
@@ -1,7 +1,9 @@
 import  {FC} from 'react';
 import Lottie from 'lottie-react';
 import registrationLottie from '../../../assets/register.json';
-import { Button, DatePicker, Form, Input, Select } from "antd";
+import { Button, DatePicker, Form, Input, message, Select, Space } from "antd";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import TextArea from "antd/lib/input/TextArea";
 
 const RegisterComponent: FC = () => {
   const onFinish = (values: any) => {
@@ -48,6 +50,62 @@ const RegisterComponent: FC = () => {
             <div className="row">
               <div className="col-md-6">
                 <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter the password",
+                      type: "string",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value: string) {
+                        if (!value || value.length >= 6) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          new Error("Password length should be more than 6!")
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input placeholder="Enter the password" />
+                </Form.Item>
+              </div>
+              <div className="col-md-6">
+                <Form.Item
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  dependencies={["password"]}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter same password",
+                      type: "string",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue("password") === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          new Error(
+                            "The two passwords that you entered do not match!"
+                          )
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input placeholder="Confirm the password" />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Form.Item
                   label="Email"
                   name="email"
                   rules={[
@@ -77,38 +135,7 @@ const RegisterComponent: FC = () => {
                 </Form.Item>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
-                <Form.Item
-                  label="Password"
-                  name="password"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please enter the password",
-                      type: "string",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Enter the password" />
-                </Form.Item>
-              </div>
-              <div className="col-md-6">
-                <Form.Item
-                  label="Password"
-                  name="password"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please enter same password",
-                      type: "string",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Confirm the password" />
-                </Form.Item>
-              </div>
-            </div>
+
             <div className="row">
               <div className="col-md-6">
                 <Form.Item
@@ -149,6 +176,164 @@ const RegisterComponent: FC = () => {
                 </Form.Item>
               </div>
             </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Form.Item
+                  label="Phone "
+                  name="mobile"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter mobile number",
+                      type: "string",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Enter Mobile Number" />
+                </Form.Item>
+              </div>
+              <div className="col-md-6">
+                <Form.Item
+                  label="Blood Group"
+                  name="blood"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter blood group",
+                      type: "string",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Enter Blood Group" />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Form.Item
+                  label="Qualification"
+                  name="qualification"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter qualification",
+                      type: "string",
+                    },
+                  ]}
+                >
+                  <Select placeholder="Select">
+                    <Select.Option value="10th">10th</Select.Option>
+                    <Select.Option value="12th">12th</Select.Option>
+                    <Select.Option value="graduate">Graduate</Select.Option>
+                    <Select.Option value="postgraduate">
+                      Post Graduate
+                    </Select.Option>
+                  </Select>
+                </Form.Item>
+              </div>
+              <div className="col-md-6">
+                <Form.Item
+                  label="Marital Status"
+                  name="maritalStatus"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please select marital status",
+                      type: "string",
+                    },
+                  ]}
+                >
+                  <Select placeholder="Select">
+                    <Select.Option value="married">Married</Select.Option>
+                    <Select.Option value="unmarried">Unmarried</Select.Option>
+                    <Select.Option value="divorce">Divorce</Select.Option>
+                    <Select.Option value="widow">Widow</Select.Option>
+                    <Select.Option value="widower">Widower</Select.Option>
+                  </Select>
+                </Form.Item>
+              </div>
+            </div>
+            <div>
+              <Form.Item
+                label="Address"
+                name="address"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter address",
+                    type: "string",
+                  },
+                ]}
+              >
+                <TextArea rows={3} placeholder="Enter Address" />
+              </Form.Item>
+            </div>
+
+            <div>
+              <Form.List name="members">
+                {(fields, { add, remove }) => (
+                  <>
+                    {fields.map(({ key, name, ...restField }) => (
+                      <Space
+                        key={key}
+                        style={{ display: "flex", marginBottom: 8 }}
+                        align="baseline"
+                      >
+                        <Form.Item
+                          {...restField}
+                          name={[name, "relationId"]}
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please enter member idhusband",
+                            },
+                          ]}
+                        >
+                          <Input placeholder="Enter Exsiting member id" />
+                        </Form.Item>
+
+                        <Form.Item
+                          {...restField}
+                          name={[name, "relationName"]}
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please select relation",
+                            },
+                          ]}
+                        >
+                          <Select>
+                            <Select.Option value="father">Father</Select.Option>
+                            <Select.Option value="mother">Mother</Select.Option>
+                            <Select.Option value="husband">
+                              Husband
+                            </Select.Option>
+                            <Select.Option value="wife">Wife</Select.Option>
+                            <Select.Option value="sister">Sister</Select.Option>
+                            <Select.Option value="brother">
+                              Brother
+                            </Select.Option>
+                          </Select>
+                        </Form.Item>
+
+                        <MinusCircleOutlined onClick={() => remove(name)} />
+                      </Space>
+                    ))}
+                    <Form.Item>
+                      <Button
+                        type="dashed"
+                        onClick={() => add()}
+                        block
+                        icon={<PlusOutlined />}
+                      >
+                        Add Member
+                      </Button>
+                    </Form.Item>
+                  </>
+                )}
+              </Form.List>
+            </div>
+
             <div>
               <Form.Item>
                 <Button type="primary" htmlType="submit">
