201a9ae99aec7ef7c331ee593f1ae427ec7af3df
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2022-08-12 12:21:35 +0530

    FIX: Fixed about page css

--- src/Components/pages/aboutPage/index.tsx
+++ src/Components/pages/aboutPage/index.tsx
@@ -1,27 +1,24 @@
-import { AntDesignOutlined } from "@ant-design/icons";
 import { Avatar, Card } from "antd";
 import Meta from "antd/lib/card/Meta";
-import React from "react";
 import "./index.scss";
+
 const AboutComponent = () => {
   return (
-    <div>
-      <div className="p-3">
-        <h1 className="text-center">Sindhi Seva Samiti</h1>
-        <p className="fs-4 mx-5 text-center">
-          Sindhi Seva Samiti was founded in 1956 as a social organization by the
-          Sindhis of Bangalore to serve the Sindhi community in Bangalore and
-          also act as a window to integrate with the local population. Starting
-          with a Darbar in the centre of the City, the Sindhi Seva Samiti is
-          today a centre for the Sindhis of Bangalore, to get together on
-          various Social and Religious occasions. The Samiti has also laid
-          emphasis in the field of Education and has a number of Educational
-          institutions with a total strength of around 6000 students.
-        </p>
-      </div>
+    <div className="container">
+      <h1 className="text-center fw-light my-md-3">Sindhi Seva Samiti</h1>
+      <p className="fs-6 text-center my-md-5">
+        Sindhi Seva Samiti was founded in 1956 as a social organization by the
+        Sindhis of Bangalore to serve the Sindhi community in Bangalore and
+        also act as a window to integrate with the local population. Starting
+        with a Darbar in the centre of the City, the Sindhi Seva Samiti is
+        today a centre for the Sindhis of Bangalore, to get together on
+        various Social and Religious occasions. The Samiti has also laid
+        emphasis in the field of Education and has a number of Educational
+        institutions with a total strength of around 6000 students.
+      </p>
       {/* card  */}
-      <div className="row align-items-center">
-        <div className="col md-3 mx-5 justify-content-center">
+      <div className="row">
+        <div className="col-md-3">
           <Card
             hoverable={true}
             className="card"
@@ -36,7 +33,7 @@ const AboutComponent = () => {
           </Card>
         </div>
 
-        <div className="col md-3 mx-5 justify-content-center">
+        <div className="col-md-3">
           <Card
             hoverable={true}
             className="card"
@@ -50,7 +47,7 @@ const AboutComponent = () => {
             <Meta title="Eye Donation" description="this is discription" />
           </Card>
         </div>
-        <div className="col md-3 mx-5 justify-content-center">
+        <div className="col-md-3">
           <Card
             hoverable={true}
             className="card"
@@ -65,7 +62,7 @@ const AboutComponent = () => {
           </Card>
         </div>
 
-        <div className="col md-3 mx-5 justify-content-center">
+        <div className="col-md-3">
           <Card
             hoverable={true}
             className="card"
@@ -81,25 +78,22 @@ const AboutComponent = () => {
         </div>
       </div>
 
-      <div className="row mx-5 my-5 align-items-center">
+      <div className="row my-5 align-items-center text-center">
         <div className="col-md-6">
-          <div className="text-center">
-            <h3>
-              "Sri Parmanand Khatter Sindhi Council of India- National
-              President, Sri Shrikanth Bhatia Fmr Vice-Chairman NCPSL, Sri
-              Sudesh Sachdev Chairman-Sindhi Academy, Sri Shyam Jumani Past
-              President- Sindhi Federation of South India and Sri J C Prakash
-              President of Sindhi Federation of South India"
-            </h3>
-          </div>
+          <p className="fw-light fs-6">
+            "&nbsp;Sri Parmanand Khatter Sindhi Council of India- National
+            President, Sri Shrikanth Bhatia Fmr Vice-Chairman NCPSL, Sri
+            Sudesh Sachdev Chairman-Sindhi Academy, Sri Shyam Jumani Past
+            President- Sindhi Federation of South India and Sri J C Prakash
+            President of Sindhi Federation of South India&nbsp;"
+          </p>
         </div>
-        <div className="col-md-6 text-center">
+        <div className="col-md-6">
           <Avatar
-            size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-            // icon={<AntDesignOutlined />}
+            size={100}
             src="http://sindhisevasamiti.org/wp-content/uploads/2015/12/president-photo-new.jpg"
           />
-          <div> MADAN DOULATRAM</div>
+          <div className="mt-3">MADAN DOULATRAM</div>
         </div>
       </div>
     </div>
