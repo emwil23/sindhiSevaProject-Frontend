0510bdcb5086a6e13c30129d5d3d750caade0306
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2022-08-13 22:08:07 +0530

    FIX: Merged

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-// const devURL = 'http://localhost:3000';
-const herokuURL = 'https://sindhi-seva-app.herokuapp.com'
-
-export default axios.create({
-    baseURL: herokuURL
-=======
 const serverUrl = {
     local: 'http://localhost:3000',
     heroku: 'https://sindhi-seva-app.herokuapp.com'
@@ -14,5 +7,4 @@ const serverUrl = {
 
 export default axios.create({
     baseURL: serverUrl.heroku
->>>>>>> 86966552f672b84907d9e29d746e8d4af606d0c1
 })
