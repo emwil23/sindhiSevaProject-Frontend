de20c7a20b2970ba19a9c549fa00e4caf2c51cf2
Anjani3 <akmitthv3@gmail.com>
2022-08-16 22:04:06 +0530

    fix: About page

--- src/Components/pages/aboutPage/index.tsx
+++ src/Components/pages/aboutPage/index.tsx
@@ -2,12 +2,6 @@ import { Avatar, Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 import "./index.scss";
 
-// const content = (
-//   <Typography>
-//     To establish, maintain, develop and aid Schools, Colleges, Polytechnics and
-//     other educational institutions, including vocational training centres.
-//   </Typography>
-
 const AboutComponent = () => {
   return (
     <div className="container">
@@ -18,16 +12,6 @@ const AboutComponent = () => {
           Expansion is Life, Contraction is Death.
         </span>
       </h1>
-
-      {/* <div className="text-center my-3 ">
-        <span className="fs-2 mt-0"style={{ color: "#0 050b3" }}>
-          Strength is Life, Weakness is Death.
-        </span>
-        <span className="fs-2 mb-0" style={{ color: "#69c0ff" }}>
-          Expansion is Life, Contraction is Death.
-        </span>
-      </div> */}
-
       <p className="fs-6 text-center my-md-5">
         Sindhi Seva Samiti was founded in 1956 as a social organization by the
         Sindhis of Bangalore to serve the Sindhi community in Bangalore and also
