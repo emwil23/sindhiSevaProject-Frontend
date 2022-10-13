5a8ad9f691c5f44294fd6b5b8cc502641b43fefa
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2022-10-14 01:34:03 +0530

    FIX: Issue adminverifed user able to see directory

--- src/services/protectedRoute.tsx
+++ src/services/protectedRoute.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { openNotification } from "./notificationService";
 
 interface Props {
     children: any
@@ -10,7 +11,9 @@ const ProtectedRoute: FC<Props> = ({ children }) => {
   
     const userLoggedIn = useSelector((state: any) => state?.isLoggedIn?.userLoggedIn);
     const memberStatus = useSelector((state: any) => state?.currentUser?.userDetails?.status);
+    const adminVerified = useSelector((state: any) => state?.currentUser?.userDetails?.adminVerified);
+    const validPerson = memberStatus === 'Active' && adminVerified === 'Accepted';
     const redirectPath = '/';
-  return userLoggedIn && memberStatus === 'Active' ? children : <Navigate to={redirectPath} replace />;
+  return userLoggedIn && validPerson ? children : <Navigate to={redirectPath} state={openNotification('Please Contact Admin for support.')} replace />;
 }
 export default ProtectedRoute;
