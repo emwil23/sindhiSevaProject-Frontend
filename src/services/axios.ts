bad369c00d5d990b47f92306d6c800cc6072cc08
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2022-10-18 19:44:50 +0530

    FIX: Added new link for backend

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverUrl = {
     local: 'http://localhost:3003',
-    heroku: 'https://sindhi-seva-app.herokuapp.com'
+    heroku: 'https://sfob-backend.herokuapp.com'
 }
 
 export default axios.create({
