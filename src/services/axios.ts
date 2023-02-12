093c5943ceb9a3b3ca7f578c5533935bea59b8b7
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2023-02-12 20:07:49 +0530

    FIX: SHIFTED FROM HERUKO TO RENDER

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const serverUrl = {
     local: 'http://localhost:3003',
-    heroku: 'https://sfob-backend.herokuapp.com'
+    render: 'https://sfob-api.onrender.com'
 }
 
 export default axios.create({
-    baseURL: serverUrl.heroku
+    baseURL: serverUrl.render
 })
